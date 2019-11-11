7d1973d0d79a7d3a0feb11b660b8a4ee6fb0acb3
30055546 <s.medhekar@westernsydney.edu.au>
2019-11-11 16:01:43 +1100

    latest changes

--- src/app/pages/lightcontrol/light-dimmer/light-dimmer.component.ts
+++ src/app/pages/lightcontrol/light-dimmer/light-dimmer.component.ts
@@ -33,20 +33,16 @@ export class LightdimmerComponent {
     ngAfterViewInit() {
         this.dragItem = document.querySelector("#item");
         this.container = document.querySelector("#container");
-        this.dragSwitch = document.querySelector("#switch");
-        this.dragSwitchContainer = document.querySelector("#switchcase")
+
 
         this.elementRef.nativeElement.querySelector('#container')
             .addEventListener('touchstart', this.dragStart.bind(this));
         this.elementRef.nativeElement.querySelector('#container')
             .addEventListener('touchmove', this.dragEvent.bind(this));
 
-        this.elementRef.nativeElement.querySelector('#switchcase')
-            .addEventListener('touchstart', this.dragStartSwitch.bind(this));
-        this.elementRef.nativeElement.querySelector('#switchcase')
-            .addEventListener('touchmove', this.dragSwitchEvent.bind(this));
+
         var demo = document.getElementById("container")
-        console.log('The top value is ' + demo.offsetTop + ' ' + demo.scrollTop);
+
         // this.elementRef.nativeElement.querySelector('#container')
         //     .addEventListener('touchend', this.dragEnd.bind(this));
 
@@ -65,11 +61,11 @@ export class LightdimmerComponent {
         if (e.type === "touchstart") {
             //this.initialX = e.touches[0].clientX - this.xOffset;
             this.initialY = e.touches[0].clientY - this.yOffset;
-            console.log('dragstart if initialY ' + this.initialY)
+
         } else {
             //this.initialX = e.clientX - this.xOffset;
             this.initialY = e.clientY - this.yOffset;
-            console.log('dragstart else initialY ' + this.initialY)
+
         }
 
         if (e.target === this.dragItem) {
@@ -78,7 +74,7 @@ export class LightdimmerComponent {
     }
 
     dragEnd(e: any) {
-        console.log('dragend if initialY ' + this.initialY)
+
         this.initialY = this.currentY;
         this.active = false;
     }
@@ -106,9 +102,9 @@ export class LightdimmerComponent {
             //     this.currentY = item.offsetTop;
             // }
             var demo = document.getElementById("container")
-            console.log('The top value is ' + demo.offsetTop);
+
             //if (this.currentY > -item.offsetTop && this.currentY < item.offsetTop) {
-            console.log('On roll currentY ' + this.currentY);
+
             this.setTranslate(0, this.currentY, this.dragItem);
             //}
         }
@@ -119,64 +115,7 @@ export class LightdimmerComponent {
         el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
     }
 
-    dragStartSwitch(e: any) {
 
-        if (e.type === "touchstart") {
-            this.initialX = e.touches[0].clientX - this.xOffset;
-        } else {
-            this.initialX = e.clientX - this.xOffset;
-        }
-
-        if (e.target === this.dragSwitch) {
-            console.log('target');
-            this.active = true;
-        }
-    }
-
-    dragEndSwitch(e: any) {
-        this.initialY = this.currentY;
-        this.active = false;
-    }
-
-    dragSwitchEvent(e: any) {
-
-        if (this.active) {
-
-            e.preventDefault();
-            var item = document.getElementById("switch");
-            console.log(`active-> ${this.currentX}, ${item.offsetLeft}, ${this.initialX}, ${e.touches[0].clientX}`);
-
-            if (e.type === "touchmove") {
-                this.currentX = e.touches[0].clientX - this.initialX;
-            } else {
-                this.currentX = e.clientX - this.initialX;
-            }
-            this.xOffset = this.currentX;
-
-            if (this.currentX < -item.offsetLeft) {
-                this.currentX = -item.offsetLeft;
-            }
-
-            if (this.currentX > item.offsetLeft) {
-                this.currentX = item.offsetLeft;
-            }
-
-            //The left and right value is -/+ and a number.  This variable will convert -ve to +ve number then to check the range.
-            var positiveValueOfCurrentX = this.currentX < 0 ? -(this.currentX) : this.currentX;
-
-            // if (positiveValueOfCurrentX <= item.offsetLeft) {
-            //set left or right endpoint for the switch
-            this.currentX = this.currentX > 0 ? item.offsetLeft : -item.offsetLeft;
-            this.setTranslate(this.currentX, 0, this.dragSwitch);
-            this.ocillateSwitch(true);
-            //}
-        }
-    }
-
-    setTranslateSwitch(xPos: any, yPos: any, el: any) {
-        yPos = 0;
-        el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
-    }
 
     ocillateSwitch(turnOn: boolean) {
         var item = document.getElementById("item");
